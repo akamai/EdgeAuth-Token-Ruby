42b9b859b879042ca3c493dd6e96f276667e1082
astinchoi <asciineo@gmail.com>
2017-02-26 22:21:25 +0900

    URL test

--- test/test_authtoken.rb
+++ test/test_authtoken.rb
@@ -52,7 +52,7 @@ class TestAuthToken < Test::Unit::TestCase
         t.escape_early = escape_early
     end
 
-    def _queryAssertEqual(path, expacted, query: '', escape_early: false, transition: false,
+    def _queryAssertEqual(path, expacted, escape_early: false, transition: false,
                           payload: nil, session_id: nil, isUrl: true)
         _token_setting('q', escape_early, transition)
         
@@ -68,7 +68,7 @@ class TestAuthToken < Test::Unit::TestCase
         assert_equal(expacted, res.code)
     end
 
-    def _cookieAssertEqual(path, expacted, query: '', escape_early: false, transition: false,
+    def _cookieAssertEqual(path, expacted, escape_early: false, transition: false,
                            payload: nil, session_id: nil, isUrl: true)
         _token_setting('c', escape_early, transition)
         if isUrl
@@ -85,7 +85,7 @@ class TestAuthToken < Test::Unit::TestCase
         assert_equal(expacted, res.code)
     end
 
-    def _headerAssertEqual(path, expacted, query: '', escape_early: false, transition: false,
+    def _headerAssertEqual(path, expacted, escape_early: false, transition: false,
                            payload: nil, session_id: nil, isUrl: true)
         _token_setting('h', escape_early, transition)
         if isUrl
@@ -106,9 +106,72 @@ class TestAuthToken < Test::Unit::TestCase
         _queryAssertEqual(query_path, "404", escape_early: escape_early, isUrl: isUrl)
         _cookieAssertEqual(cookie_path, "404", escape_early: escape_early, isUrl: isUrl)
         _headerAssertEqual(header_path, "404", escape_early: escape_early, isUrl: isUrl)
+
+        if isUrl
+            query_string="?foo=bar&hello=world"
+            _queryAssertEqual(query_path + query_string, "403", escape_early: (false==escape_early), isUrl: isUrl)
+            _cookieAssertEqual(cookie_path + query_string, "403", escape_early: (false==escape_early), isUrl: isUrl)
+            _headerAssertEqual(header_path + query_string, "403", escape_early: (false==escape_early), isUrl: isUrl)
+        end
+
+        # Transition Key Test
+        _queryAssertEqual(query_path, "404", transition: true, escape_early: escape_early, isUrl: isUrl)
+        _cookieAssertEqual(cookie_path, "404", transition: true, escape_early: escape_early, isUrl: isUrl)
+        _headerAssertEqual(header_path, "404", transition: true, escape_early: escape_early, isUrl: isUrl)
+
+        # Payload Test
+        _queryAssertEqual(query_path, "404", payload: 'SOME_PAYLOAD_DATA', escape_early: escape_early, isUrl: isUrl)
+        _cookieAssertEqual(cookie_path, "404", payload: 'SOME_PAYLOAD_DATA', escape_early: escape_early, isUrl: isUrl)
+        _headerAssertEqual(header_path, "404", payload: 'SOME_PAYLOAD_DATA', escape_early: escape_early, isUrl: isUrl)
+
+        # Session Id Test
+        _queryAssertEqual(query_path, "404", session_id: 'SOME_SESSION_ID_DATA', escape_early: escape_early, isUrl: isUrl)
+        _cookieAssertEqual(cookie_path, "404", session_id: 'SOME_SESSION_ID_DATA', escape_early: escape_early, isUrl: isUrl)
+        _headerAssertEqual(header_path, "404", session_id: 'SOME_SESSION_ID_DATA', escape_early: escape_early, isUrl: isUrl)
     end
 
+    ##########
+    # URL TEST
+    ##########
     def test_url_escape_on__ignoreQuery_yes
         _test_case_set("/q_escape_ignore", "/c_escape_ignore", "/h_escape_ignore", true, true)
     end
+
+    def test_url_escape_off__ignoreQuery_yes
+        _test_case_set("/q_ignore", "/c_ignore", "/h_ignore", false, true)
+    end
+
+    def test_url_escape_on__ignoreQuery_no
+        query_path = "/q_escape"
+        cookie_path = "/c_escape"
+        header_path = "/h_escape"
+        _test_case_set(query_path, cookie_path, header_path, true, true)
+
+        query_string="?foo=bar&hello=world"
+        _queryAssertEqual(query_path + query_string, "404", escape_early: true, isUrl: true)
+        _cookieAssertEqual(cookie_path + query_string, "404", escape_early: true, isUrl: true)
+        _headerAssertEqual(header_path + query_string, "404", escape_early: true, isUrl: true)
+    end
+
+    def test_url_escape_off__ignoreQuery_no
+        query_path = "/q"
+        cookie_path = "/c"
+        header_path = "/h"
+        _test_case_set(query_path, cookie_path, header_path, false, true)
+        
+        query_string="?foo=bar&hello=world"
+        _queryAssertEqual(query_path + query_string, "404", escape_early: false, isUrl: true)
+        _cookieAssertEqual(cookie_path + query_string, "404", escape_early: false, isUrl: true)
+        _headerAssertEqual(header_path + query_string, "404", escape_early: false, isUrl: true)
+    end
+    
+    def test_url_query_escape_on__ignore_yes_with_salt
+        query_salt_path = "/salt"
+        ats = Akamai::AuthToken.new(key: AT_ENCRYPTION_KEY, salt: AT_SALT, window_seconds: DEFAULT_WINDOW_SECONDS, escape_early: true)
+        token = ats.generateToken(url: query_salt_path)
+        uri = URI("http://#{AT_HOSTNAME}#{query_salt_path}?#{ats.token_name}?#{token}")
+        res = Net::HTTP.get_response(uri)
+        assert_equal("404", res.code)
+    end
+    ##########
 end
